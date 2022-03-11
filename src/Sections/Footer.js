363ac5ef2459cd26a1930a5101857fad874c1a70
littlelerroyy <raptorial_radiation@live.com.au>
2022-03-11 17:57:33 +1100

    Added linkedin and github icons

--- src/Sections/Footer.js
+++ src/Sections/Footer.js
@@ -1,6 +1,18 @@
+import FontAwesomeIcon from "../Elements/FontAwesomeIcon";
 const Footer = () => {
   return (
     <footer className="bg-primary text-center text-white text-lg-start">
+      <div className="text-center">
+        <h4 style={{ fontSize: "2rem" }}>Find Me On</h4>
+        <div>
+          <a href="https://github.com/littlelerroyy/" target="_blank">
+            <FontAwesomeIcon IconClassName="fa-github" />
+          </a>
+          <a href="https://linkedin.com/in/leroyb1/" target="_blank">
+            <FontAwesomeIcon IconClassName="fa-linkedin" />
+          </a>
+        </div>
+      </div>
       <div className="text-center p-3">
         © {new Date().getFullYear()} Copyright Leroy Bowmaker. All Rights
         Reserved
